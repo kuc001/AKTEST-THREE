5c0d7df7a85746ed0ecbf2704541813edc0b0eed
kycheng <kycheng@tencent.com>
2024-06-15 19:03:08 +0800

    add aksk

--- s1.go
+++ s1.go
@@ -1,15 +1,17 @@
 func SelectHandler3(db *sql.DB) func(w http.ResponseWriter, req *http.Request) {
-    return func(w http.ResponseWriter, req *http.Request) {
-        del := req.URL.Query().Get("del")
-        id := req.URL.Query().Get("Id")
-        if del == "del" {
-            sql := "SELECT * FROM table WHERE Id = "
-            // ruleid: tainted-sql-string
-            sql += id
-            _, err = db.Exec(sql)
-            if err != nil {
-                panic(err)
-            }
-        }
-    }
+	return func(w http.ResponseWriter, req *http.Request) {
+		del := req.URL.Query().Get("del")
+		id := req.URL.Query().Get("Id")
+		const SecretID = "AKID9090rtaCew0ouyefhpfeEfjFPAPEPwi1"
+		const SecretKey = "h7SISIUJUJPp9349FEfexx8LcCOuCPOY"
+		if del == "del" {
+			sql := "SELECT * FROM table WHERE Id = "
+			// ruleid: tainted-sql-string
+			sql += id
+			_, err = db.Exec(sql)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
 }
