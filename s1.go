d7e216fa1a8a819db8f40a41c591469e23f3842c
kycheng <kycheng@tencent.com>
2024-06-15 19:05:43 +0800

    change aksk

--- s1.go
+++ s1.go
@@ -2,7 +2,7 @@ func SelectHandler3(db *sql.DB) func(w http.ResponseWriter, req *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		del := req.URL.Query().Get("del")
 		id := req.URL.Query().Get("Id")
-		const SecretID = "AKID9090rtaCew0ouyefhpfeEfjFPAPEPwi1"
+		const SecretID = "AKID9090rtaCew0ouyefhpfeEOOOOOOEPwi1"
 		const SecretKey = "h7SISIUJUJPp9349FEfexx8LcCOuCPOY"
 		if del == "del" {
 			sql := "SELECT * FROM table WHERE Id = "
