922379d1135a759f0eb7a4a90c791095d6ac2214
kycheng <kycheng@tencent.com>
2024-06-15 19:01:16 +0800

    add s1

---
+++ s1.go
@@ -0,0 +1,15 @@
+func SelectHandler3(db *sql.DB) func(w http.ResponseWriter, req *http.Request) {
+    return func(w http.ResponseWriter, req *http.Request) {
+        del := req.URL.Query().Get("del")
+        id := req.URL.Query().Get("Id")
+        if del == "del" {
+            sql := "SELECT * FROM table WHERE Id = "
+            // ruleid: tainted-sql-string
+            sql += id
+            _, err = db.Exec(sql)
+            if err != nil {
+                panic(err)
+            }
+        }
+    }
+}
