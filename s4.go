bcd430f7c82244a94bff20fc96235dee1d42a90b
kycheng <kycheng@tencent.com>
2024-06-15 18:54:50 +0800

    add one file

---
+++ s4.go
@@ -0,0 +1,11 @@
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	customerId := r.URL.Query().Get("id")
+	query := "SELECT number, expireDate, cvv FROM creditcards WHERE customerId = " + customerId
+	row, _ := db.QueryContext(ctx, query)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
