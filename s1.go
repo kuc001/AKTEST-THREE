c74bf06460aaa1e6fe61409fb4bc95c7175c6069
kycheng <kycheng@tencent.com>
2024-06-17 11:33:00 +0800

    change new aksk

--- s1.go
+++ s1.go
@@ -2,8 +2,8 @@ func SelectHandler3(db *sql.DB) func(w http.ResponseWriter, req *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		del := req.URL.Query().Get("del")
 		id := req.URL.Query().Get("Id")
-		const SecretID = "test"
-		const SecretKey = "test"
+		const SecretID = "AKID1111rtaCew0ouyefhpfeEOOOOOOEPwi1"
+		const SecretKey = "h90000UJUJPp9349FEfexx8LcCOuCPOY"
 		if del == "del" {
 			sql := "SELECT * FROM table WHERE Id = "
 			// ruleid: tainted-sql-string
